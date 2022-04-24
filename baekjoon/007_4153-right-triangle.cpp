60c622d433d741cdf9014f602624ead3331af014
MrSmokyOnion <smokyonion@kakao.com>
2022-04-25 08:35:02 +0900

    updated 007_4153-right-triangle.cpp

--- baekjoon/007_4153-right-triangle.cpp
+++ baekjoon/007_4153-right-triangle.cpp
@@ -4,12 +4,24 @@ int main()
 {
     while (true)
     {
-        int a, b, c;
+        unsigned int a, b, c;
         scanf("%d %d %d", &a, &b, &c);
 
         if(a == 0 && b == 0 && c == 0)
         {
             break;
+        }
+
+        if(a > c && a > b)
+        {
+            int temp = a;
+            a = c;
+            c = temp;
+        } else if (b > c && b > a)
+        {
+            int temp = b;
+            b = c;
+            c = temp;
         }
 
         if ((a * a + b * b) == c * c)
