65e95b85db14b32629b6509d0f5ce6403effc8e7
MrSmokyonion <smokyonion@kakao.com>
2022-05-12 00:09:20 +0900

    updated 024_1929_find-prime-number.cpp

--- baekjoon/024_1929_find-prime-number.cpp
+++ baekjoon/024_1929_find-prime-number.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
-using namespace std;
+#include <cstdio>   //iostream 으로 했었는데 시간초과 떴음..
 
 int main()
 {
     int m, n, arr[1000001] = {0};
-    cin >> m >> n;
+    scanf("%d %d", &m, &n);
+
+    arr[0] = arr[1] = 1;    //1은 소수가 아니다를 안말해줬었네..
 
     for(int i = 2; i <= n; i++)
     {
@@ -18,7 +19,7 @@ int main()
     {
         if(arr[i] == 0)
         {
-            cout << i << endl;
+            printf("%d\n", i);
         }
     }
 
