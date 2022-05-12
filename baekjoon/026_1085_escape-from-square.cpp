4e7daf773126b6f50001ed56f22459742fc4bb98
MrSmokyonion <smokyonion@kakao.com>
2022-05-13 00:11:53 +0900

    updated 026_1085_escape-from-square.cpp

--- baekjoon/026_1085_escape-from-square.cpp
+++ baekjoon/026_1085_escape-from-square.cpp
@@ -5,10 +5,8 @@ int main()
     int x, y, w, h;
     scanf("%d %d %d %d", &x, &y, &w, &h);
 
-    int ans_x = (w/2) <= x ? x : w-x; 
-    ans_x = w%2 == 0 ? ans_x : ans_x-1;
-    int ans_y = (h/2) <= y ? y : h-y;
-    ans_y = h%2 == 0 ? ans_y : ans_y-1;
+    int ans_x = (w/2) >= x ? x : w-x; 
+    int ans_y = (h/2) >= y ? y : h-y;
 
     printf("%d", ans_x < ans_y ? ans_x : ans_y);
 
